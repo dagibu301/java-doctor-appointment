3f022612ff4cbb94dddb57d2efb6ab8c1853582d
David Giraldo <davidgiraldobueno@gmail.com>
2021-11-16 10:50:43 -0500

    nested

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
     static int id = 0;
-    String speciality;
-    String name;
+    private String speciality;
+    private String name;
+    private String email;
 
     Doctor() {
         id++;
@@ -20,5 +24,50 @@ public class Doctor {
 
     public void showId() {
         System.out.println("Id: " + id);
+    }
+
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+
+    public void addAvailableAppointment(Date date, String time) {
+        availableAppointments.add(new AvailableAppointment(date, time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments() {
+        return availableAppointments;
+    }
+
+    public static class AvailableAppointment {
+        private int id;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,28 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
 
         Doctor myDoctor = new Doctor("John Wick", "general");
-        myDoctor.showName();
-        myDoctor.showId();
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
 
-        Doctor mySurgeonDoctor = new Doctor("Surgeon Ryan", "surgeon");
+        System.out.println(myDoctor.getAvailableAppointments());
+
+        for (Doctor.AvailableAppointment availableAppointment : myDoctor.getAvailableAppointments()
+        ) {
+            System.out.println(availableAppointment.getDate() + " " + availableAppointment.getTime());
+        }
+
+        /*Doctor mySurgeonDoctor = new Doctor("Surgeon Ryan", "surgeon");
         mySurgeonDoctor.showId();
 
         Patient patient = new Patient("David", "david@mail.com");
         patient.setWeight(70);
         System.out.println(patient.getWeight());
-
+*/
         /*showMenu();*/
     }
 }
